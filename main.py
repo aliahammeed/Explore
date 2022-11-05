7dcbe8624b2bb93c9efb3b0497001a8395d37019
anna rose <81513151+annarose14@users.noreply.github.com>
2022-11-05 18:08:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from genericpath import commonprefix
+from http import client
+from tokenize import Token
+from discord.ext import commands
+import discord
+import os
+import dotenv
+from dotenv import load_dotenv
+load_dotenv()
+Token= os.getenv('Token')
+intents=discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client= commands.Bot(command_prefix='!',intents=intents)
+@client.event
+async def onready():
+       print(f"{client.user}is logged in ")
+
+
+
+
+
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.reply("pong")
+
+
+client.run(Token)
