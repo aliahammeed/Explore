b7fa54268c79d811dbc85cf9c9485431d41a2e59
anna rose <81513151+annarose14@users.noreply.github.com>
2022-11-05 21:13:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from genericpath import commonprefix
 from http import client
 from tokenize import Token
@@ -12,20 +11,15 @@ Token= os.getenv('Token')
 intents=discord.Intents.default()
 intents.message_content = True
 intents.members = True
-client= commands.Bot(command_prefix='!',intents=intents)
+client=commands.Bot(command_prefix='!',intents=intents)
 @client.event
 async def onready():
        print(f"{client.user}is logged in ")
-
-
-
-
-
-
+@client.command()
+async def hi(ctx):
+    await ctx.reply("hello")
 
 @client.command()
-async def ping(ctx):
-    await ctx.reply("pong")
-
-
+async def joke(ctx):
+    await ctx.reply("Get ready to LOL")
 client.run(Token)
